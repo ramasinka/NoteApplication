aa50d573a88158176c4fa519f86cda3f2886d994
justas <jkavaliauskas@gmail.com>
2017-12-18 14:58:01 +0200

    Created User and UserService

---
+++ app/src/main/java/noteapp/voltasit/lt/noteapplication/model/User.java
@@ -0,0 +1,13 @@
+package noteapp.voltasit.lt.noteapplication.model;
+
+/**
+ * Created by CodeAcademy on 2017.12.18.
+ */
+
+public class User {
+    private long id;
+    private String userName;
+    private String password;
+    private boolean emailVerified;
+
+}
---
+++ app/src/main/java/noteapp/voltasit/lt/noteapplication/services/UserService.java
@@ -0,0 +1,10 @@
+package noteapp.voltasit.lt.noteapplication.services;
+
+/**
+ * Created by CodeAcademy on 2017.12.18.
+ */
+
+public class UserService {
+
+
+}
